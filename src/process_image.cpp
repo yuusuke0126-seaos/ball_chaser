3ac3980dfe660f99a2d4139ba3c240700920ec84
Yusuke Kobayashi <y-kobayashi@seaos.co.jp>
2019-10-29 04:35:49 +0900

    Update process_image.cpp to calculate ball pos

--- src/process_image.cpp
+++ src/process_image.cpp
@@ -2,9 +2,15 @@
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
 #include <numeric>
+#include <cmath>
 
 // Define a global client that can request services
 ros::ServiceClient client;
+float max_lin_x = 0.3;
+float max_ang_z = 0.3;
+float target_distance = 0.5;
+float FOV = 4.0 / 9.0 * M_PI;
+float ball_r = 0.1;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
@@ -27,6 +33,8 @@ void process_image_callback(const sensor_msgs::Image img)
     int white_pos = 0;
     int white_count = 0;
 
+    float lin_x, ang_z;
+
     for (int i = 0; i < img.height; i++)
     {
       for (int j = 0; j < img.width; j++)
@@ -39,24 +47,37 @@ void process_image_callback(const sensor_msgs::Image img)
       }
     }
 
+    if (white_count == 0)
+    {
+      lin_x = 0.0; ang_z = 0.0;
+    }
+    else
+    {
+      float ball_pos_x = (float) ball_r*img.width/(2.0*tan(FOV/2.0)*sqrt((float)white_count/M_PI));
+      float ball_pos_y = ((float) -white_pos/white_count/img.width*2.0-1.0) * ball_pos_x * tan(FOV/2.0);
+      ROS_INFO("x_pos: %1.2f, y_pos: %1.2f", ball_pos_x, ball_pos_y);
+    }
+    
+    
+
     if (white_count > 0)
     {
       if (white_pos/white_count < img.width/3)
       {
         // send turn left srv
-        ROS_INFO("Turn left - position: %d, count: %d", white_pos/white_count, white_count);
+        // ROS_INFO("Turn left - position: %d, count: %d", white_pos/white_count, white_count);
         drive_robot(0.1, 0.2);
       }
       else if (white_pos/white_count < img.width/3*2)
       {
         // send go straight srv
-        ROS_INFO("Go straight - position: %d, count: %d", white_pos/white_count, white_count);
+        // ROS_INFO("Go straight - position: %d, count: %d", white_pos/white_count, white_count);
         drive_robot(0.125, 0.0);
       }
       else
       {
         // send turn right srv
-        ROS_INFO("Turn right - position: %d, count: %d", white_pos/white_count, white_count);
+        // ROS_INFO("Turn right - position: %d, count: %d", white_pos/white_count, white_count);
         drive_robot(0.1, -0.2);
       }
     }

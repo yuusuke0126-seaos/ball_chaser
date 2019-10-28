330794700333dc66df8924eec1f34599a6c10e99
Yusuke Kobayashi <y-kobayashi@seaos.co.jp>
2019-10-29 05:59:29 +0900

    Add propotional control

--- src/process_image.cpp
+++ src/process_image.cpp
@@ -11,6 +11,7 @@ float max_ang_z = 0.3;
 float target_distance = 0.5;
 float FOV = 4.0 / 9.0 * M_PI;
 float ball_r = 0.1;
+float kp = 0.5;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
@@ -54,39 +55,43 @@ void process_image_callback(const sensor_msgs::Image img)
     else
     {
       float ball_pos_x = (float) ball_r*img.width/(2.0*tan(FOV/2.0)*sqrt((float)white_count/M_PI));
-      float ball_pos_y = ((float) -white_pos/white_count/img.width*2.0-1.0) * ball_pos_x * tan(FOV/2.0);
+      float ball_pos_y = -((float) white_pos/white_count/img.width*2.0-1.0) * ball_pos_x * tan(FOV/2.0);
+      float ball_ang_z = atan2(ball_pos_y, ball_pos_x);
       ROS_INFO("x_pos: %1.2f, y_pos: %1.2f", ball_pos_x, ball_pos_y);
+      lin_x = - kp * (target_distance - ball_pos_x);
+      ang_z = - kp * (0.0 - ball_ang_z);
     }
     
-    
+    ROS_INFO("lin_x: %1.2f, ang_z: %1.2f", lin_x, ang_z);
+    drive_robot(lin_x, ang_z);
 
-    if (white_count > 0)
-    {
-      if (white_pos/white_count < img.width/3)
-      {
-        // send turn left srv
-        // ROS_INFO("Turn left - position: %d, count: %d", white_pos/white_count, white_count);
-        drive_robot(0.1, 0.2);
-      }
-      else if (white_pos/white_count < img.width/3*2)
-      {
-        // send go straight srv
-        // ROS_INFO("Go straight - position: %d, count: %d", white_pos/white_count, white_count);
-        drive_robot(0.125, 0.0);
-      }
-      else
-      {
-        // send turn right srv
-        // ROS_INFO("Turn right - position: %d, count: %d", white_pos/white_count, white_count);
-        drive_robot(0.1, -0.2);
-      }
-    }
-    else
-    {
-      // send stop srv
-      ROS_INFO("No white_ball, stop");
-      drive_robot(0.0, 0.0);
-    }
+    // if (white_count > 0)
+    // {
+    //   if (white_pos/white_count < img.width/3)
+    //   {
+    //     // send turn left srv
+    //     // ROS_INFO("Turn left - position: %d, count: %d", white_pos/white_count, white_count);
+    //     drive_robot(0.1, 0.2);
+    //   }
+    //   else if (white_pos/white_count < img.width/3*2)
+    //   {
+    //     // send go straight srv
+    //     // ROS_INFO("Go straight - position: %d, count: %d", white_pos/white_count, white_count);
+    //     drive_robot(0.125, 0.0);
+    //   }
+    //   else
+    //   {
+    //     // send turn right srv
+    //     // ROS_INFO("Turn right - position: %d, count: %d", white_pos/white_count, white_count);
+    //     drive_robot(0.1, -0.2);
+    //   }
+    // }
+    // else
+    // {
+    //   // send stop srv
+    //   ROS_INFO("No white_ball, stop");
+    //   drive_robot(0.0, 0.0);
+    // }
     
 }
 

11386c73cba4cc0b2293a129a68817d904421e65
Yusuke Kobayashi <y-kobayashi@seaos.co.jp>
2019-10-28 14:26:18 +0900

    Update process_image.cpp

--- src/process_image.cpp
+++ src/process_image.cpp
@@ -9,13 +9,13 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
     // Call the safe_move service and pass the requested joint angles
     if (!client.call(srv))
-        ROS_ERROR("Failed to call service safe_move");
+        ROS_ERROR("Failed to call service drive_robot");
 }
 
 // This callback function continuously executes and reads the image data
@@ -67,10 +67,6 @@ void process_image_callback(const sensor_msgs::Image img)
       drive_robot(0.0, 0.0);
     }
     
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
 }
 
 int main(int argc, char** argv)
